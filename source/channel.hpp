40eec137095176c767ed8092b0a3960281ca7072
PangHahMan <3278475092@qq.com>
2023-12-04 22:21:33 +0800

    channel

--- source/channel.hpp
+++ source/channel.hpp
@@ -54,51 +54,62 @@ public:
         _events |= EPOLLIN;
     }
     //启动写事件监控
-    void enable_write(){
+    void enable_write() {
         _events |= EPOLLOUT;
     }
     //关闭读事件监控
-    void disable_read(){
+    void disable_read() {
         _events &= ~EPOLLIN;
-    } 
+    }
     //关闭写事件监控
-    void disable_write(){
+    void disable_write() {
         _events &= ~EPOLLOUT;
     }
     //关闭所有事件监控
-    void disable_all(){
+    void disable_all() {
         _events = 0;
-    }  
+    }
     //移除所有事件监控,后边会调用EventLoop接口来移除监控
-    void remove();       
+    void remove();
     //处理事件,一旦连接触发了事件，就调用这个函数
-    void handle_event(){
+    void handle_event() {
         //EPOLLIN: 这个标志位表明对应的文件描述符上有可读取的数据。
         //EPOLLHUP: 这个标志位表明连接已经断开，或者对应的文件描述符被挂断。
         //EPOLLPRI: 这个标志位表明对应的文件描述符上有紧急的或者高优先级的数据可读取。
-        if((_revents & EPOLLIN) || (_revents & EPOLLHUP) ||(_revents & EPOLLPRI)){
-            if(_read_callback){
-                _read_callback(); 
+        if ((_revents & EPOLLIN) || (_revents & EPOLLHUP) || (_revents & EPOLLPRI)) {
+            if (_read_callback) {
+                _read_callback();
+            }
+            //不管任何事件，都调用的回调函数，在事件处理完毕后调用，刷新活跃度
+            if (_event_callback) {
+                _event_callback();
             }
         }
-        if(_revents & EPOLLOUT){
-            if(_write_callback){
+        if (_revents & EPOLLOUT) {
+            if (_write_callback) {
                 _write_callback();
             }
+           
+            if (_event_callback) {
+                _event_callback();
+            }
         }
-        if(_revents & EPOLLERR){
-            if(_error_callback){
+        if (_revents & EPOLLERR) {
+            //不管任何事件，都调用的回调函数，需要在关闭之前调用
+            if (_event_callback) {
+                _event_callback();
+            }
+            if (_error_callback) {
                 _error_callback();
             }
         }
-        if(_revents & EPOLLHUP){
-            if(_close_callback){
+        if (_revents & EPOLLHUP) {
+            if (_event_callback) {
+                _event_callback();
+            }
+            if (_close_callback) {
                 _close_callback();
             }
-        }
-        //不管任何事件，都调用的回调函数
-        if(_event_callback){
-            _event_callback();
         }
     }
 };
